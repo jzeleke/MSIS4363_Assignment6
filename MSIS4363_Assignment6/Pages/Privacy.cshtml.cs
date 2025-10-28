d60981f3fbd8753786e08b1235ecbcdc8655d937
jzeleke <jzeleke@okstate.edu>
2025-10-28 16:32:58 -0500

    Initial commit

---
+++ MSIS4363_Assignment6/Pages/Privacy.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSIS4363_Assignment6.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
